92d4322a43f90d29b23df65b47094033e4519196
SEBrown <se.brown@hotmail.com>
2015-01-27 16:43:13 +0000

    Cleaned up and updated multilaplace.cpp
    Cleaned up code a little and clarified some variable names - should make it easier to read/follow.
    
    Also added grad calculation - from Dan's code - and some basic file I/O. Should now write to two .dat files, one containing potential data and the other the E-field values.

--- code/multilaplace.cpp
+++ code/multilaplace.cpp
@@ -1,71 +1,151 @@
 #include <iostream>
 #include <cstdlib>
 #include "bitmap_image.hpp"
+
 /*************************************************************************************
 * 'multilaplace.cpp' takes a 800x600 or smaller pixel BMP filename as a command      *
 * line arguement as well as 4 different values of potentials to assign to different  * 
 * colours on the image in order of black, red, green and blue. The program then      *
 * uses the finite difference method to fill in the potential within the rest of the  *
-* image. It then outputs the matrix in the form of an 800x600 grid of values.        *                                    *
-* The program requires the library 'bitmap_image.hpp'.                               *                                                        *
+* image. It then outputs the matrix in the form of an 800x600 grid of values.        * 
+* The program requires the library 'bitmap_image.hpp'.                               *
+*                                                                                    *
+* It should be general enough to handle any user defined system. Will be improved    *
+* by allowing more than four boundary potentials and dynamic resizing of final       *
+* matrices using dimensions of input image.                                          *
+*                                                                                    *
 *************************************************************************************/
 
 using namespace std;
-float ** locations(char* file, float p1, float p2, float p3, float p4)
+
+int rows = 0, columns = 0;
+
+float ** locations(char* file, float p_black, float p_red, float p_green, float p_blue)
 {
-bitmap_image image(file); // opens the image from the commandline arguement
-if (!image){
-        cout<<"Error - Unable to open: "<<file<<endl; // error message on failure to open
+    // Opens specified bitmap image:
+    bitmap_image image(file);
+
+    // Error handling for failure to open file:
+    if (!image)
+    {
+        cout<<"Error - Unable to open: "<<file<<endl;
         return 0;
-}
-int height=image.height(),width=image.width(); // the height and width of the image in pixels is determined
-unsigned char red,green,blue;
-float** locate = new float*[width];
-for (int p=0;p<width;p++){
+    }
+
+    // Find dimensions of image in pixels:
+    int height=image.height(),width=image.width();
+
+    // Set values of global size variable:
+    rows = height;
+    columns = width;
+    
+    unsigned char red,green,blue;
+    float** locate = new float*[width];
+
+    for (int p=0;p<width;p++)
+    {
         locate[p]=new float[height];
-        for (int q=0;q<height;q++){
-                locate[p][q]=0;
-                image.get_pixel(p,q,red,green,blue); // the quantity of red, blue and green light of the pixel at position (a,b) is analysed
-                if(red==0&&blue==0&&green==0){ // if the pixel at (a,b) has no green, blue and red light
-                        locate[p][q]=p1; // black pixels are given a potential of p1
-                }
-                if(red>blue&&red>green){ // if the pixel is red
-                        locate[p][q]=p2; // the pixel is given a potential of p2
-                }
-                if(green>blue&&green>red){ // if the pixel is green
-                        locate[p][q]=p3; // the pixel is given a potential of p3
-                }
-                if(blue>red&&blue>green){ // if the pixel is blue
-                        locate[p][q]=p4; // the pixel is given a potential of p4
-                }
+        
+        for (int q = 0; q < height; q++)
+        {
+            // Initialise array entry to zero:
+            locate[p][q]=0;
+
+            // Analyse RGB values of pixel at (p,q): 
+            image.get_pixel(p,q,red,green,blue);
+
+            // Depending on colour of pixel, set potential
+            // to user defined value:
+
+            // Black pixels:
+            if(red == 0 && blue == 0 && green == 0)
+            {
+                locate[p][q] = p_black;
+            }
+            // Red pixels:
+            if(red > blue && red > green)
+            {
+                locate[p][q]=p_red;
+            }
+            // Green pixels:
+            if(green > blue && green > red)
+            {
+                locate[p][q]=p_green;
+            }
+            // Blue pixels:
+            if(blue > red && blue > green)
+            {
+                locate[p][q]=p_blue;
+            }
 
         }
+    }
+    return locate;
 }
-return locate;
-}
+
 int main(int argc, char* argv[])
 {
-float p1=atof(argv[2]),p2=atof(argv[3]),p3=atof(argv[4]),p4=atof(argv[5]); // accepts commandline arguements for 4 different potentials
-int iterations=atoi(argv[6]); // the final commandline arguement is the number of iterations
-float**u=locations(argv[1],p1,p2,p3,p4);
-for (int count=0;count<iterations;count++){
-        for (int i=1;i<799;i++){
-                for (int j=1;j<599;j++){
-                        u[i][j]=0.25*(u[i+1][j]+u[i][j+1]+u[i-1][j]+u[i][j-1]); // finite difference method applied to find potenial
-                }
-        }
-}
-for(int b=0;b<800;b++){
-        for(int a=0;a<600;a++){
-                cout<<u[a][b]; // potential values output in a 800x600 display
-        }
-cout<<endl;
-}
-for(int b=0;b<800;b++){
-        delete[] u[b]; // allocated memory removed
-}
-delete[] u;
-return 0;
-}
+    // Take command line arguments as potential boundary values,
+    // to be assigned by colour using locations(): 
+    float p_black = atof(argv[2]), p_red = atof(argv[3]);
+    float p_green = atof(argv[4]), p_blue = atof(argv[5]);
 
+    // Final command line argument is iteration count:
+    int iterations=atoi(argv[6]);
 
+    // Use locations() to build initial array with boundaries:
+    float**u=locations(argv[1], p_black, p_red, p_green, p_blue);
+
+    // Loop through desired iteration count:
+    for (int count = 0; count < iterations; count++)
+    {
+        // Loop through x coordinates 
+        for (int i = 1;i < 799; i++)
+        {
+            // Loop through y:
+            for (int j = 1; j < 599; j++)
+            {
+                // Finite Difference Method to calculate potential as
+                // average of surrounding values:
+                u[i][j]=0.25*(u[i+1][j]+u[i][j+1]+u[i-1][j]+u[i][j-1]);
+            }
+        }
+    }
+
+    // Matrices to hold potential gradient
+    // components:
+    float x_grad[600][800];
+    float y_grad[600][800];
+
+    // Loop through final potential matrix, writing data to file.
+    // Simulataneously build and write out E-field matrix.
+    for (j=0; j<=599; j++)
+    {
+        for (i=0; i<=799; i++)
+        {
+            // Write potential to file:
+            ofstream file(("potential.dat").c_str(), ios::out | ios::app);
+            file << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << u[i][j] << endl;
+
+            // Approximation of gradients:
+            x_grad[i][j] = (u[i+1][j]-u[i][j]);
+            y_grad[i][j] = (u[i][j+1]-u[i][j]);
+
+            //print out x, y, -du/dx, -du/dy in tabular form (components of E field)
+            ofstream file(("field.dat").c_str(), ios::out | ios::app);
+            file << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << -x_grad[i][j] << "\t" << setw(10) << -y_grad[i][j] << endl;
+        }
+
+    }
+
+    // Cleaning up allocated memory:
+    for(int b=0;b<800;b++)
+    {
+        delete[] u[b];
+    }
+
+    delete[] u;
+    
+    // End.
+    return 0;
+}
