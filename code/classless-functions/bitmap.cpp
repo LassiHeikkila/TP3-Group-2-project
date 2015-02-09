8772a19821e42425084b4f890dc62c7e1b493117
SEBrown <se.brown@hotmail.com>
2015-02-09 17:55:23 +0000

    Added bitmap.cpp: Modified from last.
    Fergus' code from before, but changed the return type to the array_data struct.
    
    Also added a boolean mask, which is used in the FDM function to 'protect' boundary values. Much smaller in memory than a duplicate array since it only holds 0 or 1, and should be quicker than testing and rewriting each boundary element.
    
    ar -> values[p][q] is similar to calling ar.values[p][q], but needs to be used since it's defined as a pointer.

---
+++ code/classless-functions/bitmap.cpp
@@ -0,0 +1,79 @@
+array_data * locations(char* file, float p_black, float p_red, float p_green, float p_blue)
+{
+    // Opens specified bitmap image:
+    bitmap_image image(file);
+    array_data * ar = new array_data;
+
+    // Error handling for failure to open file:
+    if (!image)
+    {
+        cout<<"Error: Unable to open '" << file << "'.";
+        return 1;
+    }
+
+    // Find dimensions of image in pixels:
+    int height = image.height(), width = image.width();
+
+    // Set values of global size variable:
+    ar.rows = height;
+    ar.columns = width;
+
+    unsigned char red,green,blue;
+    ar -> values = new float*[width];
+
+    // Declare boolean mask:
+    ar -> mask = new bool*[width];
+
+    for (int p = 0; p < width; p++)
+    {
+        ar -> values[p] = new float[height];
+        ar -> mask[p] = new bool[height];
+        
+        for (int q = 0; q < height; q++)
+        {
+            // Initialise array entry to zero:
+            ar -> values[p][q] = 0;
+
+            // Analyse RGB values of pixel at (p,q): 
+            image.get_pixel(p,q,red,green,blue);
+
+            // Depending on colour of pixel, set potential
+            // to user defined value. Also set corresponding
+            // element of bool mask to 1 for boundary pixels:
+
+            // Black pixels:
+            if(red == 0 && blue == 0 && green == 0)
+            {
+                ar -> values[p][q] = p_black;
+                ar -> mask[p][q] = true;
+            }
+            // Red pixels:
+            if(red > blue && red > green)
+            {
+                ar -> values[p][q] = p_red;
+                ar -> mask[p][q] = true;
+            }
+            // Green pixels:
+            if(green > blue && green > red)
+            {
+                ar -> values[p][q] = p_green;
+                ar -> mask[p][q] = true;
+            }
+            // Blue pixels:
+            if(blue > red && blue > green)
+            {
+                ar -> values[p][q] = p_blue;
+                ar -> mask[p][q] = true;
+            }
+            else
+            {
+                // If not boundary element, set bool to zero.
+                // This will mean the element can be overwritten.
+                ar -> mask[p][q] = false;
+            }
+
+        }
+    }
+
+    return ar;
+}
