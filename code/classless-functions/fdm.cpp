60a136caf4bbbfe04a76f0857b923027a08efb7c
SEBrown <se.brown@hotmail.com>
2015-02-11 14:54:18 +0000

    Update fdm.cpp:  Fixed the grad loops.
    Grad loops were using unnecessary steps/variables which broke them. Fixed now.

--- code/classless-functions/fdm.cpp
+++ code/classless-functions/fdm.cpp
@@ -6,29 +6,35 @@
 // std::any_of(std::begin(potentials), std::end(potentials), [&](float i){return i == u[i][j];}) == 1 //
 // #include <algorithm>    // std::any_of()                                                           //
 
+#include "headers.h"
+
 array_data * fdm(char* image, float* potentials, int iterations)
 {
     // Use locations() to build initial array with boundaries:
     array_data * sysdat;
-    sysdat = locations(image, p_black, p_red, p_green, p_blue);
+    sysdat = locations(image, potentials[0], potentials[1], potentials[2], potentials[3]);
 
     float**u = sysdat -> values;
-    bool**bound_mask = sysdat -> mask;
+//    bool**bound_mask = sysdat -> mask;
 
-    float**x_grad = sysdat -> xgrad;
-    float**y_grad = sysdat -> ygrad;
+//    float**x_grad = sysdat -> xgrad;
+//    float**y_grad = sysdat -> ygrad;
 
     // Loop through desired iteration count:
     for (int count = 0; count < iterations; count++)
     {
+        if (count % 500 == 0)
+        {
+            cout << "Iteration " << count << ".\n";
+        }
         // Loop through x coordinates 
-        for (int i = 0; i < sysdat.columns; i++)
+        for (int i = 0; i < sysdat->columns; i++)
         {
             // Loop through y:
-            for (int j = 0; j < sysdat.rows; j++)
+            for (int j = 0; j < sysdat->rows; j++)
             {
                 // Test if point is boundary value. If so, ignore:
-                if (bound_mask[i][j])
+                if (sysdat->mask[i][j])
                 {
                     continue;
                 }
@@ -41,13 +47,13 @@ array_data * fdm(char* image, float* potentials, int iterations)
                 // edge, calculate as average of four surrounding points.
 
                 // Left edge and corners:
-                if (i = 0)
+                if (i == 0)
                 {
-                    if (j = 0)
+                    if (j == 0)
                     {
                         u[i][j] = 0.5*(u[i+1][j]+u[i][j+1]);
                     }
-                    else if (j = sysdat.rows - 1)
+                    else if (j == sysdat->rows - 1)
                     {
                         u[i][j] = 0.5*(u[i+1][j]+u[i][j-1]);
                     }
@@ -57,13 +63,13 @@ array_data * fdm(char* image, float* potentials, int iterations)
                     }
                 }
                 // Right edge and corners:
-                else if (i = sysdat.columns - 1)
+                else if (i == (sysdat->columns - 1))
                 {
-                    if (j = 0)
+                    if (j == 0)
                     {
                         u[i][j] = 0.5*(u[i-1][j]+u[i][j+1]);
                     }
-                    else if (j = sysdat.rows - 1)
+                    else if (j == sysdat->rows - 1)
                     {
                         u[i][j] = 0.5*(u[i-1][j]+u[i][j-1]);
                     }
@@ -73,12 +79,12 @@ array_data * fdm(char* image, float* potentials, int iterations)
                     }
                 }
                 // Top edge:
-                else if (j = 0)
+                else if (j == 0)
                 {
                     u[i][j]=(1/3.0)*(u[i-1][j]+u[i+1][j]+u[i][j+1]);
                 }
                 // Bottom edge:
-                else if (j = (sysdat.rows - 1))
+                else if (j == (sysdat->rows - 1))
                 {
                     u[i][j]=(1/3.0)*(u[i-1][j]+u[i+1][j]+u[i][j-1]);
                 }
@@ -92,19 +98,23 @@ array_data * fdm(char* image, float* potentials, int iterations)
     }
 
     // Create 2D arrays for grad:
-    x_grad = new float*[sysdat.columns][sysdat.rows];
-    y_grad = new float*[sysdat.columns][sysdat.rows];
+    sysdat->xgrad = new float*[sysdat->columns];//[sysdat->rows];
+    sysdat->ygrad = new float*[sysdat->columns];//[sysdat->rows];
 
-    for (int j = 0; j <= (sysdat.rows - 1); j++)
+    for (int i = 0; i < (sysdat->columns - 1); i++)
     {
-        for (int i=0; i<=(sysdat.columns - 1); i++)
+        sysdat->xgrad[i] = new float[sysdat->rows];
+        sysdat->ygrad[i] = new float[sysdat->rows];
+
+        for (int j = 0; j < (sysdat->rows - 1); j++)
         {
+//            cout << "Finding grads at (" << i << "," << j << ").\n";
             // Approximation of gradients:
-            x_grad[i][j] = (u[i+1][j]-u[i][j]);
-            y_grad[i][j] = (u[i][j+1]-u[i][j]);
+            sysdat->xgrad[i][j] = -1*(u[i+1][j]-u[i][j]);
+            sysdat->ygrad[i][j] = -1*(u[i][j+1]-u[i][j]);
         }
 
     }
-
+    cout << "GRAD FINISHED.\n";
     return sysdat;
 }
