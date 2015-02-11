b5d05cdbacf2fd4eaeef397c501643ea9beb951f
SEBrown <se.brown@hotmail.com>
2015-02-11 15:00:55 +0000

    Update data_out.cpp: Faster. Still issues.
    Added couts to check progress, will be deleted before final compile.
    
    Lassi pointed out the files were being opened on every iteration of the nested loops, which was slowing the output significantly. They are now opened once before writing and closed after.
    
    Issue: Near the last iteration of field output, a memory error is thrown up. I'll check it out and fix it over the next couple of days.

--- code/classless-functions/data_out.cpp
+++ code/classless-functions/data_out.cpp
@@ -1,22 +1,42 @@
+#include "headers.h"
+
 void data_out(array_data**u, double time_elapsed, int iterations)
 {
-  // Loop through final arrays, writing data to file:
-    for (int j=0; j<=(u.rows -1); j++)
-    {
-        for (int i=0; i<=(u.columns - 1); i++)
-        {
-            // Append N against elapsed time to file:
-            ofstream file("data/runtime.dat", ios::out | ios::app);
-            file << setw(10) << iterations << "\t" << setw(10) << time_elapsed << endl;
+    cout << "Starting data_out.\n";
+    cout << "Time: " << time_elapsed << endl;
+    cout << "iterations: " << iterations << endl;
+    cout << "Value at (0,0): " << (*u)->values[0][0] << endl;
 
+    cout << "X Grad at (0,0): " << (*u)->xgrad[0][0] << endl;
+    cout << "Y Grad at (0,0): " << (*u)->ygrad[0][0] << endl;
+
+    
+    // Append N against elapsed time to file:
+    ofstream runtime("data/runtime.dat", ios::out | ios::app);
+    runtime << setw(10) << iterations << "\t" << setw(10) << time_elapsed << endl;
+    runtime.close();
+
+    ofstream potential("data/potential.dat");
+    ofstream field("data/field.dat");
+
+    // Loop through final arrays, writing data to file:
+    for (int i = 0; i < (*u)->columns; i++)
+    {
+        for (int j = 0; j < (*u)->rows; j++)
+        {
             // Write potential to file:
-            ofstream file("data/potential.dat", ios::out | ios::app);
-            file << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << u -> values[i][j] << endl;
+            potential << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << (*u) -> values[i][j] << endl;
 
             //print out x, y, -du/dx, -du/dy in tabular form (components of E field)
-            ofstream file("data/field.dat", ios::out | ios::app);
-            file << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << -1*(u -> xgrad[i][j]) << "\t" << setw(10) << -1*(u -> ygrad[i][j]) << endl;
+            field << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << (*u) -> xgrad[i][j] << "\t" << setw(10) << (*u) -> ygrad[i][j] << endl;
         }
 
+        potential << endl;
+
     }
+
+    potential.close();
+    field.close();
+
+    cout << "FILES FINISHED.\n";
 }
