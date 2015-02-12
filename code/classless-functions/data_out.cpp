6e4251497b9194e45d01c77fc3a93c57bbef4129
SEBrown <se.brown@hotmail.com>
2015-02-12 22:41:39 +0000

    data_out: Added iterations required (from struct).

--- code/classless-functions/data_out.cpp
+++ code/classless-functions/data_out.cpp
@@ -1,19 +1,13 @@
 #include "headers.h"
 
-void data_out(array_data**u, double time_elapsed, int iterations)
+void data_out(array_data**u, double time_elapsed)
 {
-    cout << "Starting data_out.\n";
-    cout << "Time: " << time_elapsed << endl;
-    cout << "iterations: " << iterations << endl;
-    cout << "Value at (0,0): " << (*u)->values[0][0] << endl;
-
-    cout << "X Grad at (0,0): " << (*u)->xgrad[0][0] << endl;
-    cout << "Y Grad at (0,0): " << (*u)->ygrad[0][0] << endl;
-
+    cout << "Solving time: " << time_elapsed << endl;
+    cout << "Iterations: " << (*u)->req_its << endl;
     
     // Append N against elapsed time to file:
     ofstream runtime("data/runtime.dat", ios::out | ios::app);
-    runtime << setw(10) << iterations << "\t" << setw(10) << time_elapsed << endl;
+    runtime << setw(10) << (*u)->req_its << "\t" << setw(10) << time_elapsed << endl;
     runtime.close();
 
     ofstream potential("data/potential.dat");
@@ -31,8 +25,10 @@ void data_out(array_data**u, double time_elapsed, int iterations)
             {
                 continue;
             }
+
             //print out x, y, -du/dx, -du/dy in tabular form (components of E field)
             field << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << (*u) -> xgrad[i][j] << "\t" << setw(10) << (*u) -> ygrad[i][j] << endl;
+
         }
 
         potential << endl;
@@ -42,5 +38,5 @@ void data_out(array_data**u, double time_elapsed, int iterations)
     potential.close();
     field.close();
 
-    cout << "FILES FINISHED.\n";
+    cout << "Files written...\n";
 }
