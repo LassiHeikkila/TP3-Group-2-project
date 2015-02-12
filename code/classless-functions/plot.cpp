3c0dfdf90faeec7e1970f17490733174c63c8c1c
SEBrown <se.brown@hotmail.com>
2015-02-12 20:46:45 +0000

    Update plot.cpp: Fixed and working.

--- code/classless-functions/plot.cpp
+++ code/classless-functions/plot.cpp
@@ -6,26 +6,30 @@
 // Options: 0 for potential plot, 1 for E-field.    //
 //////////////////////////////////////////////////////
 
+#include "headers.h"
+
 void plot(int mode, string terminal, string file_ext)
 {
     // Open GNUPlot as file to be written to:
-    FILE *pipe_gnuplot = popen("gnuplot", "w");
+    FILE *pipe_gnuplot = popen("gnuplot -persistent", "w");
 
     // Set terminal to user defined value:
     fputs(("set terminal "+terminal+"\n").c_str(), pipe_gnuplot);
 
     // Potential plot mode:
-    if (mode = 0)
+    if (mode == 0)
     {
-        fputs(("set output plots/potential."+file_ext+"\n").c_str(), pipe_gnuplot);
+        fputs(("set output 'plots/potential."+file_ext+"'\n").c_str(), pipe_gnuplot);
         fputs("set pm3d map\n", pipe_gnuplot);
-        fputs("splot 'data/potential.dat' u 1:2\n", pipe_gnuplot);
+        fputs("splot 'data/potential.dat'\n", pipe_gnuplot);
+        fputs("set out\n", pipe_gnuplot);
     }
     // E-Field plot mode:
-    else if (mode = 1)
+    else if (mode == 1)
     {
-        fputs("set output plots/field."+file_ext+"\n",pipe_gnuplot);
+        fputs(("set output 'plots/field."+file_ext+"'\n").c_str(), pipe_gnuplot);
         fputs("plot 'data/field.dat' with vectors head filled lt 3\n", pipe_gnuplot);
+        fputs("set out\n", pipe_gnuplot);
     }
 
     // Close GNUPlot:
