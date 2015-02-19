23f4807df5b0efc03a1ed987a2450dd29d7a1bfb
SEBrown <se.brown@hotmail.com>
2015-02-19 13:57:09 +0000

    Create memory.cpp
    Measures memory usage.

---
+++ code/classless-functions/memory.cpp
@@ -0,0 +1,33 @@
+#include "headers.h"
+
+int parseline(char* line)
+{
+	int i = strlen(line);
+	while (*line < '0' || *line > '9')
+	{
+		line ++;
+	}
+
+	line[i-3] = '\0';
+	i = atoi(line);
+	return i;
+}
+
+int mem_measure()
+{
+	FILE* mem = fopen("/proc/self/status","r");
+
+	int result = -1;
+	char line[128];
+
+	while (fgets(line,128,mem) != NULL)
+	{
+		if (strncmp(line,"VmSize:",7) == 0)
+		{
+			result = parseLine(line);
+			break;
+		}
+	}
+	fclose(mem);
+	return result;
+}
