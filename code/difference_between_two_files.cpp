c612c4fac9f879252a33998c062fe3ed232e4edd
LassiHeikkila <LassiHeikkila@users.noreply.github.com>
2015-02-26 13:48:57 +0000

    Added a bunch of comments
    Also made code look a bit nicer. Has timing built in and stuff.

--- code/difference_between_two_files.cpp
+++ code/difference_between_two_files.cpp
@@ -1,157 +1,44 @@
-// COMPILE WITH "-std=c++11" flag or "stoi" and "stof" may not work
-// run with ./name columns rows file1 file2
+#include "headers.h"
 
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cstdlib>
-#include <cmath>
-#include <sstream>
-#include <vector>
-
-
-int main(int argc, char *argv[])
+void data_out(array_data**u, double time_elapsed)
 {
-	int columns = atof(argv[1]); // tell program how many rows image/grid has
-	int rows = atof(argv[2]); // tell program how many columns image/grid has
-	std::string file_1 = argv[3];
-	std::string file_2 = argv[4];
-	// make a grid of size specified by command line arguments
-	std::vector< std::vector <double> > grid_a;
-	std::vector< std::vector <double> > grid_b;
-	std::vector< std::vector <double> > grid_diff;
+    cout << "Solving time: " << time_elapsed << " seconds.\n";
+    cout << "Iterations: " << (*u)->req_its << endl;
+    cout << "Writing data files...\n";
+    
+    // Append N against elapsed time to file:
+    ofstream runtime("data/runtime.dat", ios::out | ios::app);
+    runtime << setw(10) << (*u)->req_its << "\t" << setw(10) << time_elapsed << endl;
+    runtime.close();
 
-	for ( int i = 0 ; i < columns ; i ++ )
-	{
-		grid_a.push_back(std::vector<double>());
-		grid_b.push_back(std::vector<double>());
-		grid_diff.push_back(std::vector<double>());
-	}
-	for (int i = 0; i < columns; i++)
-	{
-		for (int j = 0; j < rows; j++)
-		{
-			grid_a[i].push_back(j);
-			grid_b[i].push_back(j);
-			grid_diff[i].push_back(j);
-			grid_a[i][j] = 0;
-			grid_b[i][j] = 0;
-			grid_diff[i][j] = 1;
-		}
-	}
-	std::string sLine;
-	std::ifstream data;
+    ofstream potential("data/potential.dat");
+    ofstream field("data/field.dat");
 
-// FILE_1
+    // Loop through final arrays, writing data to file:
+    for (int i = 0; i < (*u)->columns; i++)
+    {
+        for (int j = 0; j < (*u)->rows; j++)
+        {
+            // Write potential to file:
+            potential << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << (*u) -> values[i][j] << endl;
 
-	data.open (("data/"+file_1).c_str());
-	if ( data.is_open() )
-	{
-		std::string x;
-		std::string y;
-		std::string value;
+            if (i == (*u)->columns - 1 || j == (*u)->rows - 1 || i % 10 != 0 || j % 10 != 0)
+            {
+                continue;
+            }
+            //print out x, y, -du/dx, -du/dy in tabular form (components of E field)
+                field << setw(10) << i << "\t" << setw(10) << j << "\t" << setw(10) << (*u) -> xgrad[i][j] << "\t" << setw(10) << (*u) -> ygrad[i][j] << "\t" << setw(10) << (*u) -> values[i][j] << endl;
+            //  std::cout << "\rWriting values " << i << " and " << j << "." << std::flush;
+        }
 
-		std::cout << ("Data being read in from "+file_1).c_str() << "..." << std::endl;
-		while ( std::getline(data,sLine) )
-		{
-			std::stringstream data(sLine);
-			data >> x >> y >> value;
+        potential << endl;
 
-			if ( !sLine.empty() )
-			{
-			//	std::string::size_type sz;
-				int column = stoi(x);
-				int row = stoi(y);
-				double potential = stod(value);
+    }
 
-				grid_a[column][row] = potential;
-			//	std::cout << "X: " << column << "Y: " << row << " grid[x][y]" << grid[column][row] << std::endl;
-			}
-			else
-			{
-				continue;
-			//	std::cout << "empty string" << std::endl;
-			}		
-			// std::cout << "Working..." << std::endl;
-		}
-		data.close();
-		std::cout << "Data reading finished." << std::endl;
-	}
-	else 
-	{
-		std::cout << ("Unable to open "+file_1+", exiting now.").c_str() << std::endl;
-		exit(EXIT_FAILURE);
-	}
+    std::cout << std::endl;
 
-// FILE_2
+    potential.close();
+    field.close();
 
-		data.open (("data/"+file_2).c_str());
-	if ( data.is_open() )
-	{
-		std::string x;
-		std::string y;
-		std::string value;
-
-		std::cout << ("Data being read in from "+file_2).c_str() << "..." << std::endl;
-		while ( std::getline(data,sLine) )
-		{
-			std::stringstream data(sLine);
-			data >> x >> y >> value;
-
-			if ( !sLine.empty() )
-			{
-			//	std::string::size_type sz;
-				int column = stoi(x);
-				int row = stoi(y);
-				double potential = stod(value);
-
-				grid_b[column][row] = potential;
-			//	std::cout << "X: " << column << "Y: " << row << " grid[x][y]" << grid[column][row] << std::endl;
-			}
-			else
-			{
-				continue;
-			//	std::cout << "empty string" << std::endl;
-			}		
-			// std::cout << "Working..." << std::endl;
-		}
-		data.close();
-		std::cout << "Data reading finished." << std::endl;
-	}
-	else 
-	{
-		std::cout << ("Unable to open "+file_2+", exiting now.").c_str() << std::endl;
-		exit(EXIT_FAILURE);
-	}
-
-std::ofstream output;
-output.open("data/difference.dat");
-std::cout << "Calculating difference and outputting results now..." << std::endl;
-
-double epsilon = 0.000000001;
-
-for (int x = 0; x < columns ; x++)
-	{ //loop over x co-ordinates
-//		if ( x % 20 == 0)
-//		{
-//			std::cout << "x = " << x << std::endl;
-//		}
-		for (int y = 0; y < rows ; y++)
-		{ //loop over y co-ordinates
-			if ( grid_a[x][y] <= epsilon && grid_b[x][y] <= epsilon )
-			{
-				grid_diff[x][y] = 0;
-			}
-			else
-			{
-				grid_diff[x][y] = std::abs(2 * (grid_a[x][y] - grid_b[x][y]) / (grid_a[x][y]+grid_b[x][y]))*100;	
-			}
-			output << x << "\t" << y << "\t" << grid_diff[x][y] << std::endl;
-		}
-		output << std::endl;
-	}
-	output.close();
-	std::cout << "Difference calculated and results outputted." << std::endl;
-	return 0;
+    cout << "Files written.\n";
 }
