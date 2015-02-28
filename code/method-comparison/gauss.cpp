adc16847d2467743c86813e7ebf017f7af2d7671
danmfr <danrichardson95@gmail.com>
2015-02-28 15:04:48 +0000

    Update gauss.cpp

--- code/method-comparison/gauss.cpp
+++ code/method-comparison/gauss.cpp
@@ -1,10 +1,11 @@
 // Dan Richardson, 26/2/15, gauss.cpp
 
 // assesses convergence of gauss-seidel to method analytical solution
-// command line arguments: rdaius, max_its and desired convergence
+// command line arguments: radius, max_its and desired convergence
 
 // will create quantitative comparison, i.e. average difference between analytical and numerical for each method
 // plot this difference as a function of iterations to see convergence of methods
+// will implement relative convergence
 
 #include <iostream>
 #include <cstdlib>
@@ -16,26 +17,28 @@ int main(int argc, char *argv[])
 //start clock
 clock_t t0 = clock();
 
-int count = 0;
-int i, j; //declare three counters
+//declare three counters
+int i = 0, j = 0, count = 0;
 
-//choose arbitrary values, not CLIs for simplicity
-int d=30;	//plate separation
-int h=30;	//plate height
-int dx=1;	//step-size in x
-int dy=1;	//step-size in y
-int nx=30; 	//x points
-int ny=30; 	//y points
+//arbitrary values, not CLIs for simplicity
+int d = 100;	//plate separation
+int h = 100;	//plate height
+int dx = 1;	//step-size in x
+int dy = 1;	//step-size in y
+int nx = 100; 	//x points
+int ny = 100; 	//y points
 
+//get command line arguments
 float R = atof(argv[1]); 	//radius of cylinder
 int loops = atoi(argv[2]); 	//number of iterations
 float epsilon = atof(argv[3]); 	//desired convergence
 
 int V=1; //define arbitrary plate potential
 
-//declare matrices for electrostatic potential for different algorithms
+//declare matrices for electrostatic potential
+//requires second matrix to store previous values
 float gauss[nx+2][ny+2];
-float gauss_new[nx+2][ny+2]; //requires second matrix to store previous values
+float gauss_new[nx+2][ny+2];
 
 std::cout << "Gauss-Seidel method\n";
 std::cout << "-------------------\n";
@@ -82,9 +85,9 @@ int conv_count;
 
 //iterate to find potential until max its
 //or every point has converged to required level
-while ( conv_count < nx*ny && count <= loops)
+while (conv_count < nx*ny && count <= loops)
 {
-//set number of convergeny points to zero
+//set number of convergent points to zero
 conv_count = 0;
 
  //loop over grid, ignore potential at boundaries (first and last x,y)
@@ -99,16 +102,15 @@ conv_count = 0;
    	}
    	else
    	{
-    		//employ successive over-relaxation
+    		//employ gauss-seidel method
  	   	gauss_new[i][j] = 0.25 * (gauss_new[i-1][j] + gauss[i+1][j] + gauss_new[i][j-1] + gauss[i][j+1]);
    	}
 	//check convergence
-	if (sqrt(pow(gauss_new[i][j]-gauss[i][j],2)) < epsilon) { conv_count++; }
+	if (fabs(gauss_new[i][j]-gauss[i][j]) < epsilon) { conv_count++; }
   }
  }
 
- //update all of gauss
- //requires second loop
+ //update all of matrix in second loop
  for (i=1; i<=nx; i++)
  {
   for (j=1; j<=ny; j++)
