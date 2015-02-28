32fc9f2ee82b2cc650052671783fb0dbbee87719
danmfr <danrichardson95@gmail.com>
2015-02-28 15:05:32 +0000

    Update sor.cpp

--- code/method-comparison/sor.cpp
+++ code/method-comparison/sor.cpp
@@ -5,6 +5,7 @@
 
 // will create quantitative comparison, i.e. average difference between analytical and numerical for each method
 // plot this difference as a function of iterations to see convergence of methods
+// will implement relative convergence
 
 #include <iostream>
 #include <cstdlib>
@@ -16,17 +17,18 @@ int main(int argc, char *argv[])
 //start clock
 clock_t t0 = clock();
 
-int count = 0;
-int i, j; //declare three counters
+//declare three counters
+int i = 0, j = 0, count = 0;
 
-//choose arbitrary values, not CLIs for simplicity
-int d=30;	//plate separation
-int h=30;	//plate height
-int dx=1;	//step-size in x
-int dy=1;	//step-size in y
-int nx=30; 	//x points
-int ny=30; 	//y points
+//arbitrary values, not CLIs for simplicity
+int d = 100;	//plate separation
+int h = 100;	//plate height
+int dx = 1;	//step-size in x
+int dy = 1;	//step-size in y
+int nx = 100; 	//x points
+int ny = 100; 	//y points
 
+//get command line arguments
 float R = atof(argv[1]); 	//radius of cylinder
 int loops = atoi(argv[2]); 	//number of iterations
 float epsilon = atof(argv[3]); 	//desired convergence
@@ -35,9 +37,10 @@ int V=1; //define arbitrary plate potential
 
 float omega=2/(1+float(M_PI)/nx); //define optimum relaxation parameter by given formula
 
-//declare matrices for electrostatic potential for different algorithms
+//declare matrices for electrostatic potential
+//requires second matrix to store previous values
 float sor[nx+2][ny+2];
-float sor_new[nx+2][ny+2]; //requires second matrix to store previous values
+float sor_new[nx+2][ny+2];
 
 std::cout << "Successive over-relaxation\n";
 std::cout << "--------------------------\n";
@@ -84,9 +87,9 @@ int conv_count;
 
 //iterate to find potential until max its
 //or every point has converged to required level
-while ( conv_count < nx*ny && count <= loops)
+while (conv_count < nx*ny && count <= loops)
 {
-//set number of convergeny points to zero
+//set number of convergent points to zero
 conv_count = 0;
 
  //loop over grid, ignore potential at boundaries (first and last x,y)
@@ -105,12 +108,11 @@ conv_count = 0;
  	   	sor_new[i][j] = (1-omega) * sor[i][j] + omega/4 * (sor_new[i-1][j] + sor[i+1][j] + sor_new[i][j-1] + sor[i][j+1]);
    	}
 	//check convergence
-	if (sqrt(pow(sor_new[i][j]-sor[i][j],2)) < epsilon) { conv_count++; }
+	if (fabs(sor_new[i][j]-sor[i][j]) < epsilon) { conv_count++; }
   }
  }
 
- //update all of sor
- //requires second loop
+ //update all of matrix in second loop
  for (i=1; i<=nx; i++)
  {
   for (j=1; j<=ny; j++)
