9ce5bd8de850d34fadb93fec3681936286fe4ecb
danmfr <danrichardson95@gmail.com>
2015-03-02 11:25:16 +0000

    Update accuracy.sh
    Added functionality: able to run files for differing convergences, good for generating plots of CPU time, iterations or difference between analytical and numerical as functions of convergence

--- code/method-comparison/accuracy.sh
+++ code/method-comparison/accuracy.sh
@@ -3,10 +3,19 @@
 # bash script runs each method for
 # command line arguments:
 # radius, max_its, convergence
+# for decreasing convergences
 
 for file in jacobi.exe gauss.exe sor.exe checker.exe
 do
-./$file $1 $2 $3
+ for i in 1e-2 1.5e-3 1e-3 1.5e-4 1e-4 1.5e-5 1e-5
+ do
+  echo "Running" $file "for convergence" $i"..."
+  echo ""
+  ./$file $1 $2 $i
+  echo ""
+ done
 done
+
+echo "Finished!"
 
 exit
